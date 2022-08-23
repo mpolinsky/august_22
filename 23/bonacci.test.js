80f8158c934988d17daff27a63455da8e9e050c4
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-23 15:18:54 -0400

    Update bonacci.test.js

--- 23/bonacci.test.js
+++ 23/bonacci.test.js
@@ -3,28 +3,30 @@ const contains = require('./bonacci').contains
 const verify_string = require('./bonacci').verify_string
 
 
-test('bonacci returns an int',() => {
+test('returns an int',() => {
     expect(typeof fib(0)).toBe('number')
 });
 
-test('bonacci returns 0 for input 0',() => {
+test('returns 0 for input 0',() => {
     expect(fib(0)).toEqual(0)
 });
 
 
-test('bonacci returns 1 for input 1',() => {
+test('returns 1 for input 1',() => {
     expect(fib(1)).toEqual(1)
 });
 
-test('bonacci returns 1 for input 2',() => {
+test('returns 1 for input 2',() => {
     expect(fib(2)).toEqual(1)
 });
 
 
-// This worked initially with i as the loop variable
-// then at some point I added the nested func to the test
-// after the next and added two if checks to the fib func and
-// this stopped working.  Changing loop var to p fixed it.
+
+// Check several inputs using an array
+    // Note: This worked initially with i as the loop variable
+    // then at some point I added the nested func to the test
+    // after the next and added two if checks to the fib func and
+    // this stopped working.  Changing loop var to p fixed it.
 test('checks many in/out pairs', () => {
     PAIRS = [4,3,7,13,8,21,9,34]
     for (p=0, ii=PAIRS.length; p<ii; p+=2){
